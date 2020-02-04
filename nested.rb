d073b78f048937f09f6bdafc9c208e5d5d658113
Marvin Reyes <reyemarv@gmail.com>
2020-02-04 01:55:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,6 @@ def adding_matz
      }
  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
  programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
- programmer_hash
     
 end
 
